bccb30b734993cc423d87d81288483f2ea94ba00
Ian Hogg <ian.w.hogg@gmail.com>
2024-06-10 11:43:35 +0100

    Corrected top level comments and blank lines

--- java/uk/org/merg/vlcb/VlcbSpectrumModuleTypes.java
+++ java/uk/org/merg/vlcb/VlcbSpectrumModuleTypes.java
@@ -8,12 +8,6 @@ package uk.org.merg.vlcb;
 // This file is part of VLCB-Arduino project on https://github.com/SvenRosvall/VLCB-Arduino
 // Licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
 // The full licence can be found at: http://creativecommons.org/licenses/by-nc-sa/4.0/
-// 
-// 
-// 
-// 
-// 
-// 
 
 public enum VlcbSpectrumModuleTypes {
 // 
